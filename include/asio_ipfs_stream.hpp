c3383c9012584a8bb3f8b8906851ce11d7eb8e7a
Luiz Carvalhohen <luizcarvalhohen@gmail.com>
2023-07-11 21:31:49 +0000

    updated inline

--- include/asio_ipfs_stream.hpp
+++ include/asio_ipfs_stream.hpp
@@ -34,18 +34,61 @@ namespace sgns::asio::ipfs {
 	AsioIPFSStream(asio::io_service&);
 	AsioIPFSStream(asio::io_service&, NodeId& );
 
+	template<class Token>
+	inline
+	typename Result<Token, NodeId>::type
+	create(const std::string& repo_path, Token&& token) {
+            Handler<Token, NodeId> handler(std::forward<Token>(token));
+	    Result<Token, NodeId> result(handler);
+	    _create(repo_path, std::move(handler));
+	    return result.get();
+	};
+
 	// add file 
-	ContentId& addFile(const uint8_t* data, size_t size, Token&& token) const;
+	template<class Token>
+        inline
+        typename Result<Token, ContentId>::type
+	addFile(const uint8_t* data, Token&& token) {
+           Handler<Token, ContentId> handler(std::forward<Token>(token));
+	   Result<Token, ContentId> result(handler);
+	   add_(data, std::move(handler));
+	   return result.get();
+	}
+
+
 	// remove File
-        outrcome::result<bool> removeFile(const NodeId& ) const;
+	template<class Token>
+	inline
+        typename Result<Token, outrcome::result<bool>>::type
+        removeFile(const NodeId& ) {
+            Handler<Token, outrcome::result<bool>> handler(std::forward<Token>(token));
+            Result<Token, outrcome::result<bool>> result(handler);
+            remove_(node_id, std::move(handler));
+            return result.get();
+	}
+
 	// download file
-	FileContent& getFile(const NodeId& ) const;
+	template<class Token>
+	inline
+        typename Result<Token, FileContent>::type
+	getFile(const NodeId& node_id) {
+            Handler<Token, FileContent> handler(std::forward<Token>(token));
+	    Result<Token, FileContent> result(handler);
+	    get_(node_id, std::move(handler));
+	    return result.get();
+	}
 
 
 	// add file in the node
-	ContentId& operator<<(const AsioIPFSStream&, const uint8_t* path) const; 
+	template<class Token>
+        typename Result<Token, ContentId>::type
+	operator<<(const AsioIPFSStream&, const uint8_t* path); 
+
 	// cat file 
-	FileContent& operator>>(const AsioIPFSStream&, const ContentID&) const;
+	template<class Token>
+        typename Result<Token, FileContent>::type
+	operator>>(const AsioIPFSStream&, const ContentID&) const;
+
     private:
         std::unique_ptr<AsioIPFSIntf> ipfs_intf_;
 	NodeId node_id_;
@@ -53,7 +96,20 @@ namespace sgns::asio::ipfs {
 	intr::list<HandleBase, intr::constant_time_size<false>> handles;
 
 	// node creation
-	NodeId& _create() const; 
+	void
+	_create(const string& repo_path, std::function<void( const boost::system::error_code&, const NodeId&)); 
+
+	void
+	_add( const uint8_t* data
+             , std::function<void(boost::system::error_code, std::string)>);
+
+	void
+	_remove(const NodeId&,
+	        std::function<void(boost::system::error_code, std::string)>);
+
+	void
+	_get(const NodeId&,
+	     std::function<void(boost::system::error_code, std::string)>);
     };
 }
 
