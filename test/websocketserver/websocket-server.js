17dbd12d12532ca84adf532b72a6f4500455dc4a
itsafuu <sariru@gmail.com>
2023-12-22 02:57:52 -0500

    Set up http/https servers for upgrading to WS/WSS connections for file transfers. Create a WSS and WS server for testing, will possibly remove normal ws server at some point, but we could keep for compatibility

--- test/websocketserver/websocket-server.js
+++ test/websocketserver/websocket-server.js
@@ -1,9 +1,21 @@
 const WebSocket = require('ws');
 const fs = require('fs');
 const path = require('path');
+const https = require('https');
+const http = require('http');
 
-const server = new WebSocket.Server({
-    port: 8080,
+const activeSockets = [];
+const serverOptions = {
+    key: fs.readFileSync('./certs/key.pem'),
+    cert: fs.readFileSync('./certs/cert.pem')
+};
+
+const serverOptions_nonsec = {
+};
+
+// Non-secure WebSocket server
+const nonSecureServer = new WebSocket.Server({
+    noServer: true,
     verifyClient: (info, cb) => {
         console.log('WebSocket handshake received:');
         console.log('Request origin:', info.origin);
@@ -25,36 +37,75 @@ const server = new WebSocket.Server({
         activeSockets.push(socket);
 
         cb(true, 200, 'OK');
-		
-		//server.emit('resource', requestedResource);
     }
 });
 
-const activeSockets = [];
+// Secure WebSocket server
+const secureServer = new WebSocket.Server({
+    noServer: true,
+    verifyClient: (info, cb) => {
+        console.log('WebSocket handshake received:');
+        console.log('Request origin:', info.origin);
+        console.log('Request secure:', info.secure);
+        console.log('Request resource:', info.req.url);
+        console.log('Request headers:', info.req.headers);
 
-server.on('resource', (requestedResource) => {
-    // Event handler for the resource request
-    console.log('Resource requested:', requestedResource);
-
-    // Read the content of the file asynchronously
-    fs.readFile(__dirname + requestedResource, (err, data) => {
-        if (err) {
-            console.error('Error reading file:', err);
+        // Validate and sanitize the resource request
+        const sanitizedPath = sanitizeResourcePath(info.req.url);
+        if (sanitizedPath === null) {
+            // Invalid resource request, reject the connection
+            cb(false, 400, 'Bad Request');
             return;
         }
+		const requestedResource = info.req.url;
+        // Store the WebSocket connection along with the associated file path
+        const filePath = __dirname + sanitizedPath; // Adjust the base directory accordingly
+        const socket = { connection: info.req.connection, filePath: filePath };
+        activeSockets.push(socket);
 
-        // Iterate over connected clients and send the file content to each
-        server.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                console.log('Sending data');
-                //client.send(data);
-				client.send('1');
-            }
-        });
-    });
+        cb(true, 200, 'OK');
+    }
 });
 
-server.on('connection', (socket) => {
+const httpsServer = https.createServer(serverOptions, (req, res) => {
+    // Your regular HTTP server logic
+    res.writeHead(200);
+    res.end('Hello, this is a secure server!');
+});
+
+httpsServer.on('upgrade', (request, socket, head) => {
+    const pathname = request.url;
+	secureServer.handleUpgrade(request, socket, head, (ws) => {
+		secureServer.emit('connection', ws, request);
+	});
+});
+
+httpsServer.listen(8090, () => {
+    console.log('Server listening on port 8090');
+});
+
+
+const httpServer = http.createServer(serverOptions_nonsec, (req, res) => {
+    // Your regular HTTP server logic
+    res.writeHead(200);
+    res.end('Hello, this is a secure server!');
+});
+
+httpServer.on('upgrade', (request, socket, head) => {
+    const pathname = request.url;
+	console.log('Upgrade now?', pathname);
+	nonSecureServer.handleUpgrade(request, socket, head, (ws) => {
+		nonSecureServer.emit('connection', ws, request);
+	});
+});
+
+httpServer.listen(8080, () => {
+    console.log('Server listening on port 8080');
+});
+
+
+nonSecureServer.on('connection', (socket) => {
+    // Non-secure WebSocket connection logic
     console.log('Client connected');
 
     socket.on('message', (message) => {
@@ -102,20 +153,61 @@ server.on('connection', (socket) => {
 		}else {
             // Handle other types of messages as needed
         }
-    });
-
-    socket.on('close', () => {
-        console.log('Client disconnected');
-
-        // Remove the WebSocket connection from the activeSockets array
-        const index = activeSockets.findIndex((s) => s.connection === socket._socket);
-        if (index !== -1) {
-            activeSockets.splice(index, 1);
-        }
-    });
+	});
 });
 
-// Function to validate and sanitize the resource path
+secureServer.on('connection', (socket) => {
+    // Secure WebSocket connection logic
+    console.log('Client connected');
+
+    socket.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+        // Handle different types of messages
+        if (String(message) === 'GET_FILE_SIZE') {
+            // Find the associated file path for this WebSocket connection
+            const socketInfo = activeSockets.find((s) => s.connection === socket._socket);
+            if (socketInfo) {
+                const filePath = socketInfo.filePath;
+				// Log out socket information
+				console.log(`Sending file size to socket: ${socket._socket.remoteAddress}:${socket._socket.remotePort}`);
+                // Respond with the size of the file
+                fs.stat(filePath, (err, stats) => {
+                    if (err) {
+                        console.error('Error getting file size:', err);
+                        return;
+                    }
+                    const fileSize = stats.size.toString();
+					const response = `${fileSize}\n`;
+					console.log('send file size:', fileSize);
+                    socket.send(response);
+                });
+            }
+        }
+		if(String(message) === 'GET_FILE') {
+            // Find the associated file path for this WebSocket connection
+            const socketInfo = activeSockets.find((s) => s.connection === socket._socket);
+            if (socketInfo) {
+                const filePath = socketInfo.filePath;
+				// Log out socket information
+				console.log(`Sending file size to socket: ${socket._socket.remoteAddress}:${socket._socket.remotePort}`);
+                // Respond with the file
+				fs.readFile(filePath, (err, data) => {
+                    if (err) {
+                        console.error('Error getting file:', err);
+                        return;
+                    }
+					//const response = `${data}WSEOF`;
+					const delimiterBuffer = Buffer.from('WSEOF');
+					const response = Buffer.concat([data, delimiterBuffer]);
+                    socket.send(response);
+                });
+            }			
+		}else {
+            // Handle other types of messages as needed
+        }
+	});
+});
+
 function sanitizeResourcePath(resourcePath) {
     // Prevent traversal attacks by normalizing and making sure path starts with /resources/
 
