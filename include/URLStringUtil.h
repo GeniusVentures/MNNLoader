f076dfbb92894fb0500133675764d917929eb866
itsafuu <sariru@gmail.com>
2023-12-28 11:58:13 -0500

    Include file for parser

--- include/URLStringUtil.h
+++ include/URLStringUtil.h
@@ -12,6 +12,6 @@
 /// @return prefix, i.e. "https"
 extern void getURLComponents(std::string url, std::string &prefix, std::string& base, std::string& extension);
 extern void parseHTTPUrl(std::string url, std::string& host, std::string& path, std::string& port);
-extern void parseSFTPUrl(std::string url, std::string& host, std::string& path, std::string& user, std::string& pass);
+extern void parseSFTPUrl(std::string url, std::string& host, std::string& path, std::string& user, std::string& pass, std::string& key_file);
 
 #endif // URLSTRINGUTIL_H
