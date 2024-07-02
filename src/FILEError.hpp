e5f509f305ee94727f79745f65089ff89c7ca5df
sariru@gmail.com <89619301+itsafuu@users.noreply.github.com>
2024-07-01 20:18:35 -0400

    Still not quite right, exploring another way to represent these errors

--- src/FILEError.hpp
+++ src/FILEError.hpp
@@ -32,7 +32,65 @@ namespace sgns
             ERR_NOPROVBITSWAP,
             ERR_BADDHTRES
         };
-        using CustomResult = outcome::result<std::string, ErrorCode>;
+        // Define a custom policy
+        struct custom_policy {
+            template <class Impl>
+            static constexpr void wide_value_check(Impl&& self) {
+                if (!static_cast<bool>(self))
+                    throw std::runtime_error("No value present");
+            }
+
+            template <class Impl>
+            static constexpr void wide_error_check(Impl&& self) {
+                if (!self.has_error())
+                    throw std::runtime_error("No error present");
+            }
+
+            template <class Impl>
+            static constexpr void wide_exception_check(Impl&& self) {
+                if (!self.has_exception())
+                    throw std::runtime_error("No exception present");
+            }
+
+            template <class Impl>
+            static constexpr void narrow_value_check(Impl&& self) noexcept {
+                if (!static_cast<bool>(self))
+                    self.error();
+            }
+
+            template <class Impl>
+            static constexpr void narrow_error_check(Impl&& self) noexcept {
+                if (!self.has_error())
+                    self.value();
+            }
+
+            template <class Impl>
+            static constexpr void narrow_exception_check(Impl&& self) noexcept {
+                if (!self.has_exception())
+                    self.value();
+            }
+
+            // The required on_* methods
+            template <class Impl, class... Args>
+            static constexpr void on_result_construction(Impl&& self, Args &&... args) noexcept {}
+
+            template <class Impl, class... Args>
+            static constexpr void on_result_copy_construction(Impl&& self, Args &&... args) noexcept {}
+
+            template <class Impl, class... Args>
+            static constexpr void on_result_move_construction(Impl&& self, Args &&... args) noexcept {}
+
+            template <class Impl, class... Args>
+            static constexpr void on_result_in_place(Impl&& self, Args &&... args) noexcept {}
+
+            template <class Impl, class... Args>
+            static constexpr void on_result_copy_assignment(Impl&& self, Args &&... args) noexcept {}
+
+            template <class Impl, class... Args>
+            static constexpr void on_result_move_assignment(Impl&& self, Args &&... args) noexcept {}
+        };
+
+        using CustomResult = outcome::result<std::string, ErrorCode, custom_policy>;
     }
 }
 
